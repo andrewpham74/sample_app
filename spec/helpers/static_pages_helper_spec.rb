cae2e8de15ee36f032d081e3fec8871997cc3aab
Andrew Pham <andrewpham74@gmail.com>
2013-12-03 20:22:25 -0500

    Finish static pages

--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,15 +1,5 @@
-require 'spec_helper'
-
-# Specs in this file have access to a helper object that includes
-# the StaticPagesHelper. For example:
-#
-# describe StaticPagesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
-describe StaticPagesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+RSpec.configure do |config|
+ <a href="http://railstutorial.org/">Ruby on Rails Tutorial</a>
+  config.include Capybara::DSL
 end
